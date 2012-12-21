3991c81c6180da0122a6f707222314f4bdddd9fa
Erik <erik.zetterqvist@ssab.com>
2012-12-21 20:15:27 +0100

    First commit

---
+++ ServerSentEventsTest/Controllers/SSEController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+
+namespace ServerSentEventsTest.Controllers
+{
+    public class SSEController : ApiController
+    {
+        private static readonly Lazy<Timer> _timer = new Lazy<Timer>(() => new Timer(TimerCallback, null, 0, 1000));
+        private static readonly ServerSentEvent SSE = new ServerSentEvent();
+
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            Timer t = _timer.Value;
+            return SSE.Add(request);
+        }
+
+
+        private static void TimerCallback(object state)
+        {
+            Random randNum = new Random();
+            SSE.Send(randNum.Next(30, 100).ToString());
+
+            //To set timer with random interval
+            _timer.Value.Change(TimeSpan.FromMilliseconds(randNum.Next(1, 3) * 500), TimeSpan.FromMilliseconds(-1));
+
+        }
+    }
+}
---
+++ ServerSentEventsTest/ServerSentEvent.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+
+namespace ServerSentEventsTest
+{
+    internal class ServerSentEvent
+    {
+        protected List<StreamWriter> m_streams = new List<StreamWriter>();
+        protected object mLock = new object();
+
+        public HttpResponseMessage Add(HttpRequestMessage request)
+        {
+            HttpResponseMessage response = request.CreateResponse();
+            response.Content = new PushStreamContent(OnStreamAvailable, "text/event-stream");
+            return response;
+        }
+
+        protected void OnStreamAvailable(Stream stream, System.Net.Http.HttpContent content, System.Net.TransportContext context)
+        {
+            StreamWriter streamwriter = new StreamWriter(stream);
+            lock (mLock)
+            {
+                m_streams.Add(streamwriter);
+            }
+        }
+
+        public void Send(string data)
+        {
+            Send(new Message() { Data = data });
+        }
+        public void Send(string eventType, string data)
+        {
+            Send(new Message() { EventType = eventType, Data = data });
+        }
+
+        protected void Send(Message msg)
+        {
+            List<StreamWriter> toRemove = new List<StreamWriter>();
+
+            lock (mLock)
+            {
+                foreach (var s in m_streams)
+                {
+                    try
+                    {
+                        s.WriteLine(msg.ToString());
+                        s.Flush();
+                    }
+                    catch (HttpException ex)
+                    {
+                        if (ex.ErrorCode == -2147023667) // The remote host closed the connection. 
+                        {
+                            toRemove.Add(s);
+                        }
+                    }
+                }
+
+                foreach (var s in toRemove)
+                {
+                    m_streams.Remove(s);
+                }
+            }
+        }
+
+
+        protected class Message
+        {
+            public string Data { get; set; }
+            public string EventType { get; set; }
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                if (!String.IsNullOrEmpty(EventType))
+                    sb.Append("event: ").AppendLine(EventType);
+                if (!String.IsNullOrEmpty(Data))
+                    sb.Append("data: ").AppendLine(Data);
+
+                return sb.ToString();
+            }
+        }
+    }
+}
